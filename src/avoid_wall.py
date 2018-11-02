e9da8e58166a1cb4968eb1993d871c8609d11f06
Diogo Cruz <diogohmcruz@hotmail.com>
2018-11-02 11:56:48 +0000

    Avoid wall
    The robot dodges a wall.
    It doesn't stay close to the wall.
    That is the next job.

--- src/avoid_wall.py
+++ src/avoid_wall.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
+from math import cos, sin
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Range
 
 
 def laser_callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.ranges)
+    linear = 0
+    rotational = 0
+    for i, laser_distance in enumerate(data.ranges):
+        linear -= cos(data.angle_min + i * data.angle_increment) / (1.0 + laser_distance**2)
+        rotational -= sin(data.angle_min + i * data.angle_increment) / (1.0 + laser_distance**2)
+
+    linear /= len(data.ranges);
+    rotational /= len(data.ranges);
+
+    if (linear > 0.3):
+        linear = 0.3;
+    elif (linear < -0.3):
+        linear = -0.3;
+
+    vel_pub = rospy.Publisher('/robot0/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    vel_msg.linear.x = 0.3 + linear
+    vel_msg.angular.z = rotational
+    vel_pub.publish(vel_msg)
 
 
 def sonar_callback(data):
